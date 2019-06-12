152a291c8937d50cc4dee367a795279c5de071cf
mrfox1 <lsdovscky@gmail.com>
2019-06-12 12:54:58 +0300

    add expenses controller

---
+++ app/controllers/api/v1/expenses_controller.rb
@@ -0,0 +1,47 @@
+module Api
+  module V1
+    class ExpensesController < ApiController
+      before_action :set_expense, only: [:update, :destroy]
+
+      def index
+        expenses = Expense.all
+        render json: expenses
+      end
+
+      def create
+        expense = Expense.new(expense_params)
+        # test category
+        # later category_id will be sending from client
+        expense.category_id = 1
+        if expense.save
+          render json: expense, status: :created
+        else
+          render_error(expense.errors.full_messages[0], :unprocessable_entity)
+        end
+      end
+
+      def update
+        if @expense.update(expense_params)
+          render json: @expense
+        else
+          render_error(@expense.errors.full_messages[0], :unprocessable_entity)
+        end
+      end
+
+      def destroy
+        @expense.destroy
+        head :no_content
+      end
+
+      private
+
+      def set_expense
+        @expense = Expense.find(params[:id])
+      end
+
+      def expense_params
+        params.require(:expense).permit(:sum, :date, :description)
+      end
+    end
+  end
+end
