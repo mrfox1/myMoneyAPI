636ea5cd1c7f53d347abfcc9f8bfb1186ec50e43
mrfox1 <lsdovscky@gmail.com>
2019-06-13 15:52:48 +0300

    add new validate of user email
    add regular expression for checking email address formst

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  VALID_EMAIL_REG = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
   has_secure_password
   has_secure_token
 
@@ -6,6 +8,6 @@ class User < ApplicationRecord
   has_many :incomes
 
   validates :name, :email, :password, presence: true, on: :create
-  validates :email, uniqueness: true, format: /@/
+  validates :email, uniqueness: true, format: { with: VALID_EMAIL_REG }
   validates :password, length: { minimum: 8 }, on: :create
 end
